5b0b3fa8d15d4241be11b50bd920c9370be0d5a4
Lolepopie <8401103+lolepop@users.noreply.github.com>
2020-07-20 02:35:19 +0800

    fixed lock wtf moment

--- another-raytracer/World.cs
+++ another-raytracer/World.cs
@@ -141,17 +141,18 @@ namespace another_raytracer
 					Ray ray = new Ray(origin, ov.Normalized());
 
 					ThreadPool.QueueUserWorkItem(new WaitCallback(state => {
+						object[] args = state as object[];
+						var colour = CastRay(ray).Clamp(0f, 1f);
+
 						lock (locker)
 						{
-							object[] args = state as object[];
-
-							var colour = CastRay(ray).Clamp(0f, 1f);
 							World.vertices[(int)args[0]].r = colour.X;
 							World.vertices[(int)args[0]].g = colour.Y;
 							World.vertices[(int)args[0]].b = colour.Z;
 
 							e.Signal();
 						}
+
 					}), new object[] { y * ScreenWidth + x });
 
 				}
